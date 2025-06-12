585f37bf9d91a10191290c7741b2d27dc5f6071a
Legend <85244055+LegendGamer885@users.noreply.github.com>
2025-06-12 18:08:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents help_command =None)
 DB_URL = os.getenv("DATABASE_URL")
 
 @bot.event
